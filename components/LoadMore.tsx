f9ad24ba542e57e18761cd97ad4726c7a94c98c7
Minh Khoi <hoangminhkhoi245@gmail.com>
2024-02-23 12:01:07 +0700

    first commit

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,17 +1,53 @@
+"use client";
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+
+let page = 2;
+
+type AnimeComponent = JSX.Element;
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeComponent[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (inView) {
+      setIsLoading(true);
+
+      const delay = 500;
+
+      const timeOutId = setTimeout(() => {
+        fetchAnime(page).then((res) => {
+          setData([...data, ...res]);
+          page++;
+        });
+
+        setIsLoading(false);
+      }, delay);
+
+      return () => clearTimeout(timeOutId);
+    }
+  }, [data, inView, isLoading]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
+        <div ref={ref}>
+          {inView && isLoading && (
+            <Image
+              src="./spinner.svg"
+              alt="spinner"
+              width={56}
+              height={56}
+              className="object-contain"
+            />
+          )}
         </div>
       </section>
     </>
